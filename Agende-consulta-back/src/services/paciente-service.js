ae39c53f2cd77b8ca3939b2a44f47b3920253877
lucas <lucaslmota0431@gmail.com>
2021-07-31 16:16:14 -0300

    Back igual o da aula 15

---
+++ Agende-consulta-back/src/services/paciente-service.js
@@ -0,0 +1,45 @@
+class PacienteService {
+    constructor() {
+      this.pacientes = [];
+      this.id = 0;
+      this.qtd = 0;
+    }
+    
+    add(paciente) {
+      this.id++;
+      carro.id = this.id;
+      this.pacientes.push(paciente);
+    }
+    
+    getAll() {
+      return this.pacientes;
+    }
+    
+    
+  
+    getByNome(nome) {
+      return this.pacientes.filter(function (c) {
+          return c.nome == nome;
+      });
+      
+    }
+  
+    
+    update(id, paciente) {
+      this.pacientes.forEach(function (c) {
+        if (id && id == c.id) {
+          for (var i in paciente) {
+            c[i] = paciente[i];
+          }
+        }
+      });
+    }
+    
+    delete(id) {
+      this.pacientes = this.pacientes.filter(function (c) {
+        return c.id != id;
+      });
+    }
+  }
+    
+  module.exports = PacienteService;
